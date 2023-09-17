b6a4d85cd643278cb04db953896997dcd17ec5b9
Pedro1949 <142362192+Pedro1949@users.noreply.github.com>
2023-09-16 23:02:11 -0500

    Add files via upload

---
+++ 06.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        double megabytes = 23;
+        double kilobytes = megabytes * 1024;
+        System.out.println(megabytes + " MB son equivalentes a " + kilobytes + " KB.");
+        
+    }
+}
